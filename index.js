a904c708cc09cdcd01e85ed57b90ed97cfe8948b
EseAlli <essealli.ea@gmail.com>
2022-11-10 06:17:36 +0100

    active state styling

--- index.js
+++ index.js
@@ -5,13 +5,27 @@ const nameInput = document.querySelector(".card-name");
 const cvcInput = document.querySelector(".cvc");
 const cvc = document.querySelector(".CVC");
 const form = document.querySelector("form");
+const year = document.querySelector(".year");
+const month = document.querySelector(".month");
+const dateError = document.querySelector(".date small");
+
+function checkValidity() {
+  let pattern = /[abc]/g;
+  let cardNo = numberInput.value;
+  console.log(cardNo);
+  console.log(cardNo.match(pattern));
+  if (cardNo.match(pattern)) {
+    numberInput.classList.add = "error";
+    numberInput.nextElementSibling.style.display = "block";
+  }
+}
 
 form.addEventListener("submit", (event) => {
   event.preventDefault();
+  checkValidity();
 });
 
 numberInput.addEventListener("change", () => {
-  console.log(numberInput.value.length);
   cardNumber.innerHTML = numberInput.value;
   if (numberInput.value.length > 0) {
     if (
@@ -31,3 +45,23 @@ nameInput.addEventListener("change", () => {
 cvcInput.addEventListener("change", () => {
   cvc.innerHTML = cvcInput.value;
 });
+
+function checkBlank() {
+  if (this.value === "") {
+    this.classList.add = "error";
+    this.nextElementSibling.style.display = "block";
+
+    if (this.classList.contains("year")) {
+      dateError.style.display = "block";
+    }
+  } else {
+    this.classList.remove = "error";
+    this.nextElementSibling.style.display = "none";
+  }
+}
+
+cvcInput.addEventListener("focus", checkBlank);
+
+year.addEventListener("focus", checkBlank);
+
+month.addEventListener("focus", checkBlank);
